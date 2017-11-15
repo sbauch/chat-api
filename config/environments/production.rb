de29998597c4f80057db30ff90d932bb6f6ba094
Sam Bauch <sbauch@gmail.com>
2017-11-15 16:27:05 -0500

    Remove action_mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "chat-service_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery
